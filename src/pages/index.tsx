dd8967fef146daf47634224e92353fccbf003bf0
Jose Romary <joseromarybrandao@gmail.com>
2022-02-03 13:21:43 -0300

    setup finalizado

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      
+      <Head>
+        <title>Marvel Universe</title>
+      </Head>
+
+      <main>
+       <h1>Hello Romary</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
